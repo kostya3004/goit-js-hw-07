67ce004431985398bdbe4d5ccba170145523ea41
kostya3004 <110536627+kostya3004@users.noreply.github.com>
2023-01-20 21:40:52 +0100

    1 stage

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageGallery = document.querySelector(".gallery")
+
+const markup = galleryItems.map(item =>
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`).join("")
+
+imageGallery.insertAdjacentHTML('beforeend', markup)
+
+console.log(basicLightbox)
